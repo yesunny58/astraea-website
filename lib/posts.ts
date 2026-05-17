ea89f0431386cd72952349dd5dee1c2d7f0d40db
yesunny58 <yesunny58@hotmail.com>
2026-05-18 00:58:30 +0800

    delete category in post

--- lib/posts.ts
+++ lib/posts.ts
@@ -8,6 +8,3 @@ export function getPostById(id: string) {
   return posts.find((item) => item.id === id)
 }
 
-export function getPostByCategory(category: string) {
-  return posts.filter((item) => item.category === category)
-}
